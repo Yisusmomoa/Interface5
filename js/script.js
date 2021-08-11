b2cab03158322f6613597607c3d1309af220151d
Yisusmomoa <bjls29@hotmail.com>
2021-08-11 04:29:27 -0500

    se agrega popup para iniciar sesión

--- js/script.js
+++ js/script.js
@@ -129,6 +129,26 @@ btncerrarpopup.addEventListener('click',function(){
 
 
 
+//Iniciar sesión
+//popup
+var btnAbrirPopup2=document.getElementById('btn-abrir-popup2'),
+    overlay2=document.getElementById('overlay2'),
+    popup2=document.getElementById('popup2'),
+    btncerrarpopup2=document.getElementById('btn-cerrar-popup2');
+
+    btnAbrirPopup2.addEventListener('click', function(){
+    overlay2.classList.add('active');
+    popup2.classList.add('active');
+});
+
+
+  btncerrarpopup2.addEventListener('click',function(){
+  overlay2.classList.remove('active');
+  popup2.classList.remove('active');
+});
+
+
+
 
 
 
